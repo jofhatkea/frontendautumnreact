006eabda9cffff56130c52d737103f18e59e6486
Jonas Holbech <jofh@kea.dk>
2019-10-17 10:21:17 +0200

    Updates

---
+++ precache-manifest.6c902a1977c71c369ae32201edaaead8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e08b28eb5a0dc9412421f84f334fc640",
+    "url": "/frontendautumnreact/index.html"
+  },
+  {
+    "revision": "7029536a3423215b76ed",
+    "url": "/frontendautumnreact/static/css/main.16be0329.chunk.css"
+  },
+  {
+    "revision": "63deb5d3a393be6c7df4",
+    "url": "/frontendautumnreact/static/js/2.d7b84754.chunk.js"
+  },
+  {
+    "revision": "7029536a3423215b76ed",
+    "url": "/frontendautumnreact/static/js/main.61d522f0.chunk.js"
+  },
+  {
+    "revision": "7b4fe27c7fb793d7d8d5",
+    "url": "/frontendautumnreact/static/js/runtime-main.2cc009e3.js"
+  }
+]);
